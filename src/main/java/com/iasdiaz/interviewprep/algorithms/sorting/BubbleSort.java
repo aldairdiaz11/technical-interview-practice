54999957e71143f76fe4f0087039eb9d295d2869
aldairdiaz <irving.s21diaz@gmail.com>
2026-01-30 14:26:52 -0600

    Add bubble sort implementation

---
+++ src/main/java/com/iasdiaz/interviewprep/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.iasdiaz.interviewprep.algorithms.sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length == 0) return;
+        boolean swapping = true;
+        while (swapping) {
+            swapping = false;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    swapping = true;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
